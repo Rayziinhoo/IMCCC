9b2ecc5b1370b130ffb0dbe6a91a94f1d3a5864f
RAYRISON RIBEIRO ESTACIO <rayrison.estacio@alunos.sc.senac.br>
2026-03-05 20:45:03 -0300

    I

---
+++ src/main/java/com/imc/calcular/Controller/ImcController.java
@@ -0,0 +1,10 @@
+package com.imc.calcular.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/imc")
+public class ImcController {
+}
---
+++ src/main/java/com/imc/calcular/DTOS/Dto.java
@@ -0,0 +1,5 @@
+package com.imc.calcular.DTOS;
+
+
+public class Dto {
+}
